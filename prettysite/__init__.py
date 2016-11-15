cd161a5b95e8bc0553bbee5f1eaaab896a516c25
Andrew Scott <a.clayton.scott@gmail.com>
2016-11-14 20:52:06 -0600

    Broke out config into its own file

--- prettysite/__init__.py
+++ prettysite/__init__.py
@@ -1,11 +1,7 @@
-import os
-
 from flask import Flask
+import config as config
 from flask_sqlalchemy import SQLAlchemy
 from logging.handlers import RotatingFileHandler
-
-
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__, static_url_path='')
 
@@ -13,13 +9,6 @@ db = SQLAlchemy(app)
 
 import models
 import views
-
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'prettyunit.db')
-app.config['DEBUG'] = False
-app.config['VERSION'] = '0.1-ALPHA'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['LOG_PATH'] = os.path.join(basedir, 'logs/prettyunit.log')
 
 
 
