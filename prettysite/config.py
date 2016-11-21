6948ed66a078ad3d8c28404025c54f074794cbff
Andrew Scott <a.clayton.scott@gmail.com>
2016-11-20 18:57:24 -0600

    More deployment tweaks

--- prettysite/config.py
+++ prettysite/config.py
@@ -8,4 +8,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = False
 VERSION = '0.1-ALPHA'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-LOG_PATH = os.path.join(basedir, 'logs/prettyunit.log')
+LOG_PATH = 'logs/prettyunit.log'
